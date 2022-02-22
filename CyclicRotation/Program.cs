7d25157862b671e8c6c71c38590414d727577fb6
FetraRanaivoson <ranaivosonfetra@gmail.com>
2022-02-22 16:03:01 -0500

    Cyclic Rotation

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace CyclicRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 3, 8, 9, 7, 6 };
+            int K = 3;
+
+            Console.WriteLine(solution(A, K));
+        }
+
+        public static int[] solution(int[] A, int K)
+        {
+            // Store the last element of the array
+            int temp = A[A.Length - 1];
+
+            // for K times
+            for (int i = 0; i < K; i++)
+            {
+                // for each element of the array, move each element to right
+                // except for the first element
+                for (int index = A.Length - 1; index > 0; index--)
+                {
+                    A[index] = A[index - 1];
+                }
+
+                // Replace the first element by the stored element
+                A[0] = temp;
+
+                // Do not forget to reset the temp to the new last element of the array
+                temp = A[A.Length - 1];
+            }
+
+            Console.Write("[");
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i != A.Length - 1)
+                    Console.Write(A[i] + ", ");
+                else
+                    Console.Write(A[i]);
+            }
+            Console.WriteLine("]");
+
+            return A;
+        }
+    }
+}
