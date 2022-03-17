8e4c59cf7c0dbc113c087a48c14d24b25bc19d3a
FetraRanaivoson <ranaivosonfetra@gmail.com>
2022-03-17 12:38:24 -0400

    Graph class: implemented Breadth First Search method

--- Graph-Class/Program.cs
+++ Graph-Class/Program.cs
@@ -7,21 +7,22 @@ namespace Graph_Class
     public class Graph
     {
         int nbNodes;
-        Dictionary<int, Connection> adjacencyList;
+        Dictionary<int, Vertex> adjacencyList;
+
         public Graph()
         {
             this.nbNodes = 0;
-            this.adjacencyList = new Dictionary<int, Connection>();
+            this.adjacencyList = new Dictionary<int, Vertex>();
         }
 
         /// <summary>
         /// Add a vertex or a node to this graph.The node will be stored as a Key in an hash table
         /// </summary>
-        public void AddVertex(int newVertexKey)
+        public void AddVertex(int vertexValue)
         {
-            if (!this.adjacencyList.ContainsKey(newVertexKey))
+            if (!this.adjacencyList.ContainsKey(vertexValue))
             {
-                this.adjacencyList.Add(newVertexKey, new Connection());
+                this.adjacencyList.Add(vertexValue, new Vertex(vertexValue)); //Note that the key and the vertex value is the same
                 this.nbNodes++;
             }
         }
@@ -33,24 +34,29 @@ namespace Graph_Class
         {
             //  Sanity check
             int failedToAddEdgeCount = 0;
+
+            //Node vertexANode = new Node(vertexA);
             if (!this.adjacencyList.ContainsKey(vertexA))
             {
                 Console.WriteLine("Vertex '{0}' doesn't exists on the graph!", vertexA);
                 failedToAddEdgeCount++;
             }
+
+            //Node vertexBNode = new Node(vertexB);
             if (!this.adjacencyList.ContainsKey(vertexB))
             {
                 Console.WriteLine("Vertex '{0}' doesn't exists on the graph!", vertexB);
                 failedToAddEdgeCount++;
             }
+
             if (failedToAddEdgeCount > 0)
             {
                 return;
             }
 
-            //  Add edge operation
-            this.adjacencyList[vertexA].AddConn(vertexB, vertexB); //Note that the connection key is the same as the value
-            this.adjacencyList[vertexB].AddConn(vertexA, vertexA); //Note that the connection key is the same as the value
+            //  Add the edges 
+            this.adjacencyList[vertexA].AddConn(vertexB, this.adjacencyList[vertexB]);
+            this.adjacencyList[vertexB].AddConn(vertexA, this.adjacencyList[vertexA]);
         }
 
         /// <summary>
@@ -64,6 +70,63 @@ namespace Graph_Class
         }
 
         /// <summary>
+        /// Make a Breadth First Search in this graph starting from provided vertex. 
+        /// BFS in graph searches for all the neighbours first then deeper last. 
+        /// NB: the memory consumption for the queue can be very large if the tree is very wide
+        /// </summary>
+        /// <param name="startVertex">The starting vertex of the BFS</param>
+        /// <returns>The list of order of visit for a BFS</returns>
+        public List<int> BreadthFirstSearchR(int startVertex)
+        {
+            // Initialize
+            foreach (KeyValuePair<int, Vertex> vertexKey in adjacencyList)
+            {
+                vertexKey.Value.IsVisited = false;
+            }
+
+            Queue<Vertex> vertexQueue = new Queue<Vertex>();
+            List<int> list = new List<int>();
+
+            if (!this.adjacencyList.ContainsKey(startVertex))
+            {
+                Console.WriteLine("BFS Error : the graph doesn't contains the vertex {0}", startVertex);
+                return null;
+            }
+
+            vertexQueue.Enqueue(adjacencyList[startVertex]);
+            adjacencyList[startVertex].IsVisited = true;
+
+            return BreadthFirstSearchRecursive(vertexQueue, list);
+
+        }
+        /// <summary>
+        /// The recursive method for a BFS.
+        /// </summary>
+        /// <param name="queue">The list of direct neighbours to visit</param>
+        /// <param name="list">The list of already visited vertices</param>
+        private List<int> BreadthFirstSearchRecursive(Queue<Vertex> queue, List<int> list)
+        {
+            if (queue.Count == 0)
+            {
+                Console.Write("Graph BFS visit order starting from {0}: ", list[0]);
+                Console.WriteLine(string.Join(", ", list));
+                return list;
+            }
+            Vertex currentVertex = queue.Dequeue();
+            list.Add(currentVertex.value);
+            currentVertex.IsVisited = true;
+
+            foreach (KeyValuePair<int, Vertex> vertexKey in currentVertex.connections)
+            {
+                if (!vertexKey.Value.IsVisited)
+                {
+                    queue.Enqueue(vertexKey.Value); //BFS => Priority is the NEIGHBOURS (currentVertex.connections) !
+                }
+            }
+            return BreadthFirstSearchRecursive(queue, list);
+        }
+
+        /// <summary>
         /// Print all the connections for this graph
         /// </summary>
         public void Print()
@@ -72,10 +135,10 @@ namespace Graph_Class
                 return;
 
             Console.WriteLine("Graph connections: ");
-            foreach (KeyValuePair<int, Connection> vertex in adjacencyList)
+            foreach (KeyValuePair<int, Vertex> vertex in adjacencyList)
             {
                 Console.Write("Vertex {0} ---> ", vertex.Key);
-                if(vertex.Value.connections.Count == 0)
+                if (vertex.Value.connections.Count == 0)
                 {
                     Console.Write("No connections");
                 }
@@ -91,21 +154,30 @@ namespace Graph_Class
         }
     }
 
-    public class Connection
-    {
-        public Dictionary<int, int> connections;
+    /// <summary>
+    /// A vertex (Node) prototype for graphs that implement BFS and DFS
+    /// </summary>
 
-        public Connection()
+    public class Vertex
+    {
+        public int value;
+        public Dictionary<int, Vertex> connections;
+        public bool IsVisited { get; set; }
+
+
+        public Vertex(int value)
         {
-            this.connections = new Dictionary<int, int>();
+            this.value = value;
+            this.IsVisited = false;
+            this.connections = new Dictionary<int, Vertex>();
         }
 
         /// <summary>
-        /// Add a connection using key value pair <int,int> to the dictionary
+        /// Add a connection using key value pair <int,Vertex> to the dictionary
         /// </summary>
-        public void AddConn(int key, int value)
+        public void AddConn(int key, Vertex vertex)
         {
-            connections.Add(key, value);
+            connections.Add(key, vertex);
         }
     }
 
@@ -139,6 +211,11 @@ namespace Graph_Class
 
 
             graph.Print();
+
+            graph.BreadthFirstSearchR(0); //0,2,3,5, 1,4
+            graph.BreadthFirstSearchR(3); //3,1,2,4, 0,5
+            graph.BreadthFirstSearchR(5); //5,2,0, 3,1,4
+
         }
     }
 }
