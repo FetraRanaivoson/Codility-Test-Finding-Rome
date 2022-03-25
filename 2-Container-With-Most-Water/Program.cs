88c38d95bbfa3bbb76b7c3bbcfae9ba5eb38f938
FetraRanaivoson <ranaivosonfetra@gmail.com>
2022-03-25 00:05:43 -0400

    Container with most water: improved time complexity to O(n)

--- 2-Container-With-Most-Water/Program.cs
+++ 2-Container-With-Most-Water/Program.cs
@@ -57,9 +57,14 @@ namespace Container_With_Most_Water
             Console.WriteLine("Greatest area of water (Bubble sort): {0}", MaxAreaContainer(A));
             Console.WriteLine(sw.ElapsedTicks);
 
+            //2100 ticks
+            var sw2 = Stopwatch.StartNew();
+            Console.WriteLine("Greatest area of water (O(n)): {0}", MaxAreaContainer2(A));
+            Console.WriteLine(sw2.ElapsedTicks);
+
         }
 
-        private static int MaxAreaContainer(int[] a) // O(N^2)
+        private static int MaxAreaContainer(int[] a) // O(n^2)
         {
             List<int> areaList = new List<int>();//=> Duplicate values
 
@@ -69,7 +74,14 @@ namespace Container_With_Most_Water
                 {
                     if (i != j)
                     {
-                        if (a[j] > a[i])
+                        if (a[j] == a[i])
+                        {
+                            int w = Math.Abs(j - i);
+                            int h = a[i]; // or a[j]
+                            if (!areaList.Contains(w * h))
+                                areaList.Add(w * h);
+                        }
+                        else if (a[j] > a[i])
                         {
                             int w = Math.Abs(j - i);
                             int h = a[i];
@@ -89,5 +101,61 @@ namespace Container_With_Most_Water
             areaList.Sort();
             return areaList[areaList.Count - 1];
         }
+
+
+        private static int MaxAreaContainer2(int[] v)
+        {
+            //     9
+            //  8  |   8
+            //  |**|   ^
+            //  |6*|   |
+            //  ||*|   |
+            //  ||*|4  |
+            //  ||*||  |
+            //  ||2||  |
+            // 1|||||  |
+            // ||||||  |
+            // 012345
+
+            //  Initialize the max area
+            //  While v[i] < v[next] result can grow
+            //  Specifically, if v[i] < v[lastIndex], optimum result would be v[i], v[lastIndex], a = v[i] * lastIndex = 1*4 = 4
+            //   => So I don't need to check the in between: I know that I need to compare to a
+            int maxArea = v[0] * (v.Length - 1 - 0);
+            int highestWallValue = -1;
+            int highestWallIndex = -1;
+
+            if (v[0] < v[v.Length - 1])
+            {
+                highestWallValue = v[v.Length - 1];
+                highestWallIndex = v.Length - 1;
+            }
+            else
+            {
+                highestWallValue = v[0];
+                highestWallIndex = 0;
+            }
+
+            for (int i = 1; i < v.Length; i++)
+            {
+                //  So I go to v[i+1]:8 (> 4 so can potentially improve the solution) so compare to the last one: v[i+1]= 8 > v[lastIndex] = 4 so a= v[lastIndex] * i+1 = 4* 4 = 16
+                if (v[i] >= highestWallValue)
+                {
+                    maxArea = highestWallValue * Math.Abs(highestWallIndex - i);
+                    highestWallValue = v[i];
+                    highestWallIndex = i;
+                }
+
+                //  Now check v[i+1]: 6 (< 8 so cannot potentially improve the solution)
+                //  So I compare to v[i+1]: 2 (< 8 so cannot potentially improve the solution)
+                //  So I compare to v[i+1]: 9 (> 8 so can potentially improve the solutionà a = 8*indexof9 = 8* 4 = 24 > a
+                //  So I compare tp v[i+1]: 4 (<9 so cannot potentially improve the solution)
+            }
+            return maxArea;
+        }
+
+
     }
 }
+
+
